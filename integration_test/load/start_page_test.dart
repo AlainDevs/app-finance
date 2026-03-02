906a11560bb4a21236ec805bcdaffb2ed1b0c07a
AlainDevs <cv112113@gmail.com>
2026-03-02 14:48:18 +0000

    [#8] [BF] Enhance E2E reliability by adding Chromedriver setup and cleanup in workflow

--- integration_test/load/start_page_test.dart
+++ integration_test/load/start_page_test.dart
@@ -2,27 +2,26 @@
 // Use of this source code is governed by a CC BY-NC-ND 4.0 license that can be found in the LICENSE file.
 
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_gherkin_wrapper/flutter_gherkin_wrapper.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'package:app_finance/_configs/test_keys.dart';
-
-import '../../test/e2e/e2e_test.list.dart';
-import '../../test/e2e/_steps/given/first_run.dart';
-import '../../test/pump_main.dart';
+import 'package:app_finance/main.dart' as app;
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
-  ExecutableStepIterator.inject(classList);
 
   testWidgets('Cover Starting Page', (WidgetTester tester) async {
     final pref = await SharedPreferences.getInstance();
     await pref.clear();
-    await PumpMain.init(tester, true);
 
-    FileRunner.tester = tester;
-    await FirstRun().executeStep();
+    app.main();
+
+    final loading = find.byKey(TestKeys.homeInitializationPage);
+    while (loading.evaluate().isNotEmpty) {
+      await tester.pumpAndSettle();
+    }
+    await tester.pumpAndSettle();
 
     expect(find.byKey(TestKeys.startPage), findsOneWidget);
   });
